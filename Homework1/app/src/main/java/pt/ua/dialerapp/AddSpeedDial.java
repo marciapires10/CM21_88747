f72f3aff6f3758c46b60cb59ad21f2ab8fc0180b
Marcia <marcia.pires@ua.pt>
2021-12-04 16:24:45 +0000

    First full functional version

---
+++ Homework1/app/src/main/java/pt/ua/dialerapp/AddSpeedDial.java
@@ -0,0 +1,71 @@
+package pt.ua.dialerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddSpeedDial extends AppCompatActivity {
+
+    // Defining edit texts
+    EditText name, phone_number;
+
+    // Defining button
+    Button submit, speed_dial1, speed_dial2, speed_dial3;
+
+    // Shared Preferences
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_speed_dial);
+
+        sharedPreferences = this.getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+
+        String button_speed_dial = sharedPreferences.getString("button", "");
+        Log.d("nyTag", button_speed_dial);
+
+        // edit texts inputs
+        name = findViewById(R.id.name);
+        phone_number = findViewById(R.id.phone);
+
+        // buttons
+        submit = findViewById(R.id.submitSD);
+
+
+        submit.setOnClickListener(v -> {
+            sharedPreferences.edit().putString("button", button_speed_dial).apply();
+            sharedPreferences.edit().putString("name_" + button_speed_dial, name.getEditableText().toString()).apply();
+            sharedPreferences.edit().putString("phone_number_" + button_speed_dial, phone_number.getEditableText().toString()).apply();
+
+            Toast.makeText(AddSpeedDial.this, "Speed dial saved successfully!", Toast.LENGTH_SHORT).show();
+
+            //sharedPreferences.edit().apply();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("buttonText", name.getEditableText().toString());
+            startActivity(intent);
+        });
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        sharedPreferences = this.getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+
+        String button_speed_dial = sharedPreferences.getString("button", "");
+        String name_sh = sharedPreferences.getString("name_" + button_speed_dial, "");
+        String phone_number_sh = sharedPreferences.getString("phone_number_" + button_speed_dial, "");
+
+        name.setText(name_sh);
+        phone_number.setText(phone_number_sh);
+    }
+}
---
+++ Homework1/app/src/main/java/pt/ua/dialerapp/MainActivity.java
@@ -0,0 +1,192 @@
+package pt.ua.dialerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Defining buttons
+    Button button1, button2, button3, button4, button5, button6, button7, button8, button9,
+            button_star, button0, button_hash, button_memory1, button_memory2, button_memory3;
+
+    // Defining image buttons
+    ImageButton clear_number, call;
+
+    // Defining text view
+    TextView phone_number;
+
+    // Defining Permission codes.
+    // We can give any value
+    // but unique for each permission.
+    private static final int REQUEST_PHONE_CALL = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+
+        // buttons
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
+        button_star = findViewById(R.id.buttonstar);
+        button0 = findViewById(R.id.button0);
+        button_hash = findViewById(R.id.buttonhash);
+        button_memory1 = findViewById(R.id.memory1);
+        button_memory2 = findViewById(R.id.memory2);
+        button_memory3 = findViewById(R.id.memory3);
+
+        button_memory1.setText(sharedPreferences.getString("name_memory1", ""));
+        button_memory2.setText(sharedPreferences.getString("name_memory2", ""));
+        button_memory3.setText(sharedPreferences.getString("name_memory3", ""));
+
+
+        // image buttons
+        clear_number = findViewById(R.id.clearnumber);
+        call = findViewById(R.id.call);
+
+        // text view
+        phone_number = findViewById(R.id.phonenumber);
+
+
+        // Set buttons on Click Listeners
+        button1.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"1"));
+
+        button2.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"2"));
+
+        button3.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"3"));
+
+        button4.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"4"));
+
+        button5.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"5"));
+
+        button6.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"6"));
+
+        button7.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"7"));
+
+        button8.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"8"));
+
+        button9.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"9"));
+
+        button_star.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"*"));
+
+        button0.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"0"));
+
+        button_hash.setOnClickListener(v -> phone_number.setText(phone_number.getText().toString()+"#"));
+
+        // Click Listener to make a phone call
+        call.setOnClickListener(v -> makePhoneCall(false, ""));
+
+        // Click Listener to delete numbers on text view
+        clear_number.setOnClickListener(v -> {
+            if(phone_number.length() != 0){
+                StringBuilder stringBuilder = new StringBuilder(phone_number.getText());
+                stringBuilder.deleteCharAt(phone_number.getText().length()-1);
+
+                String new_string = stringBuilder.toString();
+                phone_number.setText(new_string);
+            }
+            else {
+                phone_number.setText("");
+            }
+        });
+
+        button_memory1.setOnLongClickListener(v -> {
+            SharedPreferences sharedPreferences1 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            sharedPreferences1.edit().putString("button", "memory1").apply();
+            startActivity(new Intent(getApplicationContext(), AddSpeedDial.class));
+            return true;
+        });
+
+        button_memory1.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences12 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            String number = sharedPreferences12.getString("phone_number_memory1", "");
+            makePhoneCall(true, number);
+        });
+
+        button_memory2.setOnLongClickListener(v -> {
+            SharedPreferences sharedPreferences13 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            sharedPreferences13.edit().putString("button", "memory2").apply();
+            startActivity(new Intent(getApplicationContext(), AddSpeedDial.class));
+            return true;
+        });
+
+        button_memory2.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences14 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            String number = sharedPreferences14.getString("phone_number_memory2", "");
+            makePhoneCall(true, number);
+        });
+
+        button_memory3.setOnLongClickListener(v -> {
+            SharedPreferences sharedPreferences15 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            sharedPreferences15.edit().putString("button", "memory3").apply();
+            startActivity(new Intent(getApplicationContext(), AddSpeedDial.class));
+            return true;
+        });
+
+        button_memory3.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences16 = getSharedPreferences("pt.ua.dialerapp", MODE_PRIVATE);
+            String number = sharedPreferences16.getString("phone_number_memory3", "");
+            makePhoneCall(true, number);
+        });
+
+    }
+
+    public void makePhoneCall(boolean speed_dial, String number){
+
+        if (!speed_dial) {
+            number = phone_number.getText().toString();
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PHONE_CALL);
+        }
+        else{
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+number)));
+        }
+
+    }
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        String number = phone_number.getText().toString();
+
+        if (requestCode == REQUEST_PHONE_CALL){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    return;
+                }
+                else {
+                    startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+number)));
+                }
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Call Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
